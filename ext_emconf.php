e089ba40373f5a1f9f21a9f6049015a227ae95fd
Albrecht Köhnlein <ak@koehnlein.eu>
2016-07-27 10:00:48 +0200

    Copy from TYPO3 Extension Repository (TER)

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tt_calender"
+#
+# Auto generated 11-05-2008 09:37
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Calendar',
+	'description' => 'Enter dates in the tables and they can be displayed on the webpage. Very simple. Not so useful.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasperYYYY@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.0.5',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:12:"ext_icon.gif";s:4:"ef1e";s:15:"ext_icon__h.gif";s:4:"ce83";s:16:"ext_icon__ht.gif";s:4:"587c";s:15:"ext_icon__t.gif";s:4:"662a";s:15:"ext_icon__x.gif";s:4:"4555";s:14:"ext_tables.php";s:4:"0328";s:14:"ext_tables.sql";s:4:"2f1b";s:28:"ext_typoscript_constants.txt";s:4:"651e";s:24:"ext_typoscript_setup.txt";s:4:"417c";s:25:"locallang_csh_ttcalen.php";s:4:"7da2";s:26:"locallang_csh_ttcalenc.php";s:4:"79d6";s:17:"locallang_tca.php";s:4:"0581";s:7:"tca.php";s:4:"7b52";s:14:"doc/manual.sxw";s:4:"30a8";s:15:"pi/calendar.inc";s:4:"9f59";s:25:"pi/calendar_template.tmpl";s:4:"af0f";}',
+);
+
+?>
